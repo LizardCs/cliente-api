5ba914e0fca4d06447d078ab278ebb6a9f6d872b
Johan Curicho <johancuricho935@gmail.com>
2025-08-03 16:54:16 -0500

    Se agrego Conexiòn cruzada para pruebas con Angular

--- src/main/java/com/minegocio/cliente_api/controller/ClienteController.java
+++ src/main/java/com/minegocio/cliente_api/controller/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")  //Conexion con Angular
 @RestController
 @RequestMapping("/api/clientes")
 @RequiredArgsConstructor
